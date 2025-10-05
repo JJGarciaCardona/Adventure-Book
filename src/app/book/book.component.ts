99e2ac393872b2a5215e330fc90b6098b358f7b3
jafetga <eli.jafet.ga@gmail.com>
2025-10-05 16:03:05 -0600

    feat: enhance book component with navigation buttons and improved animations

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -10,8 +10,48 @@ import { CommonModule } from '@angular/common';
 })
 export class BookComponent {
   isCoverOpen = false;
+  currentPage = 0;
+  totalPages = 6;
+  closingPage: number | null = null;
 
   toggleCover() {
     this.isCoverOpen = !this.isCoverOpen;
+    if (this.isCoverOpen) {
+      this.currentPage = 1;
+    } else {
+      this.currentPage = 0;
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.closingPage = null; // Resetea la página que se cierra
+      if (this.currentPage === 0) {
+        // Si el libro está cerrado, primero lo abrimos
+        this.isCoverOpen = true;
+      }
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 0) {
+      this.closingPage = this.currentPage; // Marca la página actual para la animación de cierre
+      if (this.currentPage === 1) {
+        // Si estamos en la primera página, cerramos la cubierta
+        this.isCoverOpen = false;
+      }
+      this.currentPage--;
+    }
+  }
+
+  // Método para determinar si una página específica debe voltearse
+  shouldTurn(pageIndex: number): boolean {
+    return this.isCoverOpen && pageIndex <= this.currentPage;
+  }
+
+  // Método para determinar si una página se está cerrando
+  isClosing(pageIndex: number): boolean {
+    return this.closingPage === pageIndex;
   }
 }
