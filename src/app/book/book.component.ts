1570b14a7a7d80c5c163693dce0402c19182f125
jafetga <eli.jafet.ga@gmail.com>
2025-10-05 14:47:15 -0600

    feat: add book component with cover animation and update app layout
    - Added a new standalone BookComponent with cover and page animations.
    - Updated app.component.html to include the BookComponent.
    - Modified app.component.ts to import and use the BookComponent.
    - Added styles for the BookComponent in book.component.css.
    - Updated global styles in styles.css to set a background color.
    - Disabled Angular CLI analytics in angular.json.

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-book',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './book.component.html',
+  styleUrl: './book.component.css'
+})
+export class BookComponent {
+  isCoverOpen = false;
+
+  toggleCover() {
+    this.isCoverOpen = !this.isCoverOpen;
+  }
+}
