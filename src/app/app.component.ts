59296a8058e2e2ed43e3ecb546a84aff5800d26f
jafetga <eli.jafet.ga@gmail.com>
2025-10-05 21:56:52 -0600

    Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DemoComponent } from "./demo/demo.component";
+import { BookComponent } from "./book/book.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,  DemoComponent],
+  imports: [RouterOutlet, BookComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,28 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PageComponent } from "../page/page.component";
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [CommonModule, PageComponent],
+  imports: [CommonModule],
   templateUrl: './book.component.html',
-  styleUrl: './book.component.css'
+  styleUrl: './book.component.css',
 })
-export class BookComponent {
-  isCoverOpen = false;
-  initialState = true;
-
-  nextPage() {
-    if (!this.isCoverOpen) {
-      this.isCoverOpen = true;
-      this.initialState = false;
-    }
-  }
-
-  prevPage() {
-    if (this.isCoverOpen) {
-      this.isCoverOpen = false;
+export class BookComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const book = document.getElementById('book');
+    if (book) {
+      book.addEventListener('click', function () {
+        this.style.pointerEvents = 'none';
+        setTimeout(() => {
+          this.style.pointerEvents = '';
+        }, 2000);
+      });
     }
   }
 }
